088824fc541488ca8402579267c9f0b573785ac3
Yehezkel Pernas <areyes@nerd.com>
2019-06-26 19:16:20 -0400

    temporal commit

--- task.go
+++ task.go
@@ -18,6 +18,24 @@ func (t TaskApi) Fetch(id string) (*TodoItem, error) {
 	return out.Task, nil
 }
 
+func (t TaskApi) Update(id string, task *EditTaskPayload) (*TodoItem, error) {
+
+	var out interface{}
+	payload := struct {
+		task *EditTodoItem `json:"todo-item"`
+	}{
+		task,
+	}
+
+	err := t.TaskEndPoint.Update(id, payload, &out)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return out.Task, nil
+}
+
 type TaskEndPoint struct {
 	Client ApiClient
 }
@@ -35,6 +53,13 @@ func (t TaskEndPoint) Fetch(id string, out interface{}) error {
 	return nil
 }
 
+func (t TaskEndPoint) Update(id string, in, out interface{}) error {
+
+	// to simple no need for string interpolation here
+	endpoint := "tasks/" + id + ".json"
+	return t.Client.DoRequest("PUT", endpoint, in, out)
+}
+
 type BoardColumn struct {
 	Id    int
 	Name  string
@@ -50,4 +75,13 @@ type TodoItem struct {
 type TaskResponse struct {
 	Status string    `json:"STATUS"`
 	Task   *TodoItem `json:"todo-item"`
+}
+
+type EditTaskPayload struct {
+	Status string        `json:"STATUS"`
+	Task   *EditTodoItem `json:"todo-item"`
+}
+
+type EditTodoItem struct {
+	ColumnId int `json:"columnId"`
 }
